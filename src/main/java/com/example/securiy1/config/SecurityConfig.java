ea1453772157c5baf0fbb6888b66eee215ebe58d
hwang-yeongil <jyhwangy4k@gmail.com>
2022-12-16 16:51:12 +0900

    chapter8 end 12/16

--- src/main/java/com/example/securiy1/config/SecurityConfig.java
+++ src/main/java/com/example/securiy1/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.securiy1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,7 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록 됨
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig{
+	
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
 	
 	@Bean
 	public BCryptPasswordEncoder encoderPwd() {
@@ -29,7 +35,11 @@ public class SecurityConfig{
 			.loginPage("/loginForm")
 			.loginProcessingUrl("/login")
 			.defaultSuccessUrl("/")
-			;
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm")
+			.userInfoEndpoint()//1 코드받기(인증, 2 엑서스토큰(권한, 3 프로필 정보 가져옴, 4 정보를 토대로 회원가입 
+			.userService(principalOauth2UserService);
 		
 		return http.build();
 	}
