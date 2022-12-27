6120f6bd5bb7946c8dc73c050b41f262c3d6820e
hwang-yeongil <jyhwangy4k@gmail.com>
2022-12-27 16:58:16 +0900

    12/27

--- src/main/java/com/example/securiy1/config/auth/PrincipalDetails.java
+++ src/main/java/com/example/securiy1/config/auth/PrincipalDetails.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.securiy1.model.User;
 
+import lombok.Data;
+
+@Data
 public class PrincipalDetails implements UserDetails{
 	
 	private User user;
--- src/main/java/com/example/securiy1/controller/IndexController.java
+++ src/main/java/com/example/securiy1/controller/IndexController.java
@@ -3,12 +3,17 @@ package com.example.securiy1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.securiy1.config.auth.PrincipalDetails;
 import com.example.securiy1.model.User;
 import com.example.securiy1.repository.UserRepository;
 
@@ -20,6 +25,24 @@ public class IndexController {
 	
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@GetMapping("/test/login")
+	public @ResponseBody String testLogin(Authentication authentication,
+			@AuthenticationPrincipal PrincipalDetails userDetails){
+		System.out.println("/test/login==================");
+		PrincipalDetails principalDetails = (PrincipalDetails)authentication.getPrincipal();
+		System.out.println("authentication : " + principalDetails.getUser());
+		System.out.println("userDetails : "+ userDetails.getUser());
+		return "세션 정보 확인하기";
+	}
+	@GetMapping("/test/oauth/login")
+	public @ResponseBody String testOAuthLogin(Authentication authentication, @AuthenticationPrincipal OAuth2User oauth){
+		System.out.println("/test/oauth/login==================");
+		OAuth2User oauth2User = (OAuth2User)authentication.getPrincipal();
+		System.out.println("authentication : " + oauth2User.getAttributes());
+		System.out.println("oauth2User : "+ oauth.getAttributes());
+		return "OAuth 세션 정보 확인하기";
+	}
 	
 	@GetMapping({"","/"})
 	public String index() {
