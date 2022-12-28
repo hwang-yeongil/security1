9f823ba3f2bdf11806a2a347c0140648c6eb0521
hwang-yeongil <jyhwangy4k@gmail.com>
2022-12-28 16:57:28 +0900

    12/28 BCryptPasswordEncoder WARN

--- src/main/java/com/example/securiy1/config/PrincipalOauth2UserService.java
+++ src/main/java/com/example/securiy1/config/PrincipalOauth2UserService.java
@@ -1,22 +1,56 @@
 package com.example.securiy1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import com.example.securiy1.config.auth.PrincipalDetails;
+import com.example.securiy1.model.User;
+import com.example.securiy1.repository.UserRepository;
+
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
-
+	
+//	@Autowired
+//	private BCryptPasswordEncoder bCryptPasswordEncoder;
+//	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		System.out.println("getClientRegistration: " +userRequest.getClientRegistration());
 		System.out.println("getAccessToken: " +userRequest.getAccessToken().getTokenValue());
 		System.out.println("getAttributes: " +super.loadUser(userRequest).getAttributes());
 		
-		OAuth2User oAuth2User = super.loadUser(userRequest);
-		return super.loadUser(userRequest);
+		OAuth2User oauth2User = super.loadUser(userRequest);
+		System.out.println("getAttributes: " + oauth2User.getAttributes());
+		
+		String provider = userRequest.getClientRegistration().getClientId();
+		String providerId = oauth2User.getAttribute("sub");
+		String username = provider +"_"+providerId;
+		String password = "겟인데어";
+		String email = oauth2User.getAttribute("email");
+		String role = "ROLE_USER";
+		
+		User userEntity = userRepository.findByUsername(username);
+		if(userEntity == null) {
+			userEntity = User.builder()
+					.username(username)
+					.password(password)
+					.email(email)
+					.role(role)
+					.provider(provider)
+					.providerId(providerId)
+					.build();
+			userRepository.save(userEntity);
+		}
+		
+		return new PrincipalDetails(userEntity, oauth2User.getAttributes());
 	}
 	
 	
--- src/main/java/com/example/securiy1/config/auth/PrincipalDetails.java
+++ src/main/java/com/example/securiy1/config/auth/PrincipalDetails.java
@@ -2,21 +2,30 @@ package com.example.securiy1.config.auth;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import com.example.securiy1.model.User;
 
 import lombok.Data;
 
 @Data
-public class PrincipalDetails implements UserDetails{
+public class PrincipalDetails implements UserDetails, OAuth2User{
 	
 	private User user;
+	private Map<String, Object> attribute;
 	
+	// 일반 로그인
 	public PrincipalDetails(User user) {
 		this.user = user;
+	}
+	// OAuth 로그인 
+	public PrincipalDetails(User user,Map<String, Object> attribute) {
+		this.user = user;
+		this.attribute = attribute;
 	}
 	
 	@Override
@@ -67,6 +76,19 @@ public class PrincipalDetails implements UserDetails{
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
 		return true;
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		// TODO Auto-generated method stub
+		return attribute;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+//		return attribute.get("sub")
+		return null;
 	}
 	
 }
